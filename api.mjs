32b6c4c344d2ceea2eb4de53c5610ed5fbd3e359
Sahil Bajaj <1797328+spinningarrow@users.noreply.github.com>
2022-03-30 20:25:09 +0200

    Add some react and stub api stuff

---
+++ api.mjs
@@ -0,0 +1,11 @@
+export const search = async (query) => [];
+
+export const getSeasons = async (showId) => {
+  console.log("getting seasons for show", showId);
+};
+
+export const addShow = async (showId, seasonId) => {};
+
+export const startWatching = async (showId, seasonId, startDate) => {};
+
+export const endWatching = async (showId, seasonId, endDate) => {};
